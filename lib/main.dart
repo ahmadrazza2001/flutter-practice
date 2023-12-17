7f51588ec5b17a39c98060ca80e9bda130b61acb
ahmadrazza2001 <126485111+ahmadrazza2001@users.noreply.github.com>
2023-12-17 22:46:36 +0500

    assignment 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // Import Firebase Core
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:practice/Lab7.dart';
 import 'package:practice/Lab7T2.dart';
-
 import 'package:practice/page2.dart';
 import 'package:practice/LoginForm.dart';
 import 'user_provider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     ChangeNotifierProvider(
       create: (context) => UserProvider(),
@@ -26,99 +28,109 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'Roboto',
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Colors.deepPurple,
-                  Colors.purple,
-                  Colors.blue,
-                ],
-              ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.deepPurple,
+                Colors.purple,
+                Colors.blue,
+              ],
             ),
           ),
-          title: const Text(
-            "MAD Tasks",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
-            ),
+        ),
+        title: const Text(
+          "MAD Tasks",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
           ),
-          centerTitle: true,
-          elevation: 0,
         ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          children: <Widget>[
-            _buildCard(FontAwesomeIcons.bookOpen, "Lab-5", Page2(), context),
-            _buildCard(
-                FontAwesomeIcons.signInAlt, "Login", LoginForm(), context),
-            _buildCard(FontAwesomeIcons.android, "Lab-7 T1", Lab7(), context),
-            _buildCard(FontAwesomeIcons.android, "Lab-7 T2", Lab7T2(), context),
-          ],
-        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: <Widget>[
+          _buildCard(FontAwesomeIcons.bookOpen, "Lab-5", Page2()),
+          _buildCard(FontAwesomeIcons.signInAlt, "Login", LoginForm()),
+          _buildCard(FontAwesomeIcons.android, "Employees", Lab7()),
+          _buildCard(FontAwesomeIcons.android, "Lab-7 T2", Lab7T2()),
+        ],
       ),
     );
   }
 
-  Widget _buildCard(
-      IconData icon, String label, Widget route, BuildContext context) {
-    return Card(
-      elevation: 4,
-      margin: EdgeInsets.all(10),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Builder(
-        builder: (BuildContext context) {
-          return InkWell(
+  Widget _buildCard(IconData icon, String label, Widget route) {
+    return Builder(
+      builder: (context) {
+        return Card(
+          elevation: 4,
+          margin: EdgeInsets.all(10),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: InkWell(
             onTap: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => route),
+                MaterialPageRoute(
+                  builder: (context) => route,
+                ),
               );
             },
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Colors.deepPurple,
-                    Colors.purple,
-                    Colors.blue,
+            child: Hero(
+              tag: label, // Unique tag for each card
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Colors.deepPurple,
+                      Colors.purple,
+                      Colors.blue,
+                    ],
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      icon,
+                      size: 60,
+                      color: Colors.white,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      label,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ],
                 ),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(
-                    icon,
-                    size: 60,
-                    color: Colors.white,
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    label,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
               ),
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
